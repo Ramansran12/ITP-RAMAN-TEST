023ce49a1199a4bee434c4b8810b4247565d1768
“Ramansran12” <ramansran0501@gmail.com>
2023-03-31 10:51:25 -0400

    v1

--- Problem1.py
+++ Problem1.py
@@ -1,15 +1,15 @@
 #Ramandeep Singh
 #id 22076254
 # define values
-def calculate_bill (clean, cavity, xray):
+def calculate_bill (cavity, xray, clean):
     total_bill = 0
     
-    if clean == 'y':
-        total_bill += 60
     if cavity == 'y':
         total_bill += 200
     if xray == 'y':
         total_bill += 100
+    if clean == 'y':
+        total_bill += 60
     
     # total bill with tax
     total_bill *= 1.15
@@ -27,10 +27,10 @@ def calculate_bill (clean, cavity, xray):
     return (total_bill, discount)
 
 # putting input of user
-patient_name = input("Enter patient's name: ")
-cleaning_performed = input("Was cleaning performed? (y/n): ")
-cavity_filling_performed = input("Was cavity-filling performed? (y/n): ")
-xray_performed = input("Was X-Ray performed? (y/n): ")
+patient_name = input("Enter patient's name:")
+cleaning_performed = input("Was cleaning performed? (y/n):")
+cavity_filling_performed = input("Was cavity-filling performed? (y/n):")
+xray_performed = input("Was X-Ray performed? (y/n):")
 
 #calculate bill as well as discount
 bill_amount, discount = calculate_bill(cleaning_performed, cavity_filling_performed, xray_performed)
